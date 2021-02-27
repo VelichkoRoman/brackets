c30b07808151bc47a5148bb0f746ead4583bd08b
velichkoroman <tehvel@mail.ru>
2021-02-27 22:04:17 +0300

    feat: add my version brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newStr = str;
+  for (let i = 0; i < str.length / 2; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      newStr = newStr.replace(bracketsConfig[j][0] + bracketsConfig[j][1], '');
+    }
+  }
+  return newStr.length === 0;
 }
